1f0f29b59820a3b01b4f393a244fc55c56b2c4e6
zibs <elizibin@gmail.com>
2016-02-02 17:03:18 -0800

    Add initial models and validations:
    Add Category model, Post model, Comment model, and Contact model along with their various validations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160203005454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "subject"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
---
+++ spec/factories/contacts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :contact do
+    email "MyString"
+name "MyString"
+subject "MyString"
+message "MyText"
+  end
+
+end
